2d90eb61347473c2883ca7107cc315b19057a29e
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-18 06:58:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(sentance)
-
+  forward_sentance = string.split(" ")
+    return_array = []
+    forward_sentance.each do |sentance|
+      return_array << sentance.reverse
+    end
+    return_array.join(" ")
 end
