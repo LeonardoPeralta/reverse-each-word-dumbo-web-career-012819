09191107f824ed815969abf254ec85a84227c931
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-18 07:01:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,17 @@
-def reverse_each_word(sentance)
-  forward_sentance = sentance.split(" ")
+def reverse_each_word(sentece)
+  forward_sentance = sentence.split(" ")
     return_array = []
-    forward_sentance.each do |sentance|
-      return_array << sentance.reverse
+    forward_sentance.each do |sentence|
+      return_array << sentence.reverse
     end
     return_array.join(" ")
+end
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|sentence| #iterate over the array
+    test_array << sentence.reverse #reverse each word in the array
+  end
+  test_array.join(" ")
 end
